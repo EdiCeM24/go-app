efa7c1e21d763721fe20b43eb4b7700230347f2e
Marcus Edidiong Cletus <edi_mars@outlook.com>
2023-01-13 12:29:33 -0800

    instsall go=app go.mod main.go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main 
+
+//import 'fmt'
+
+func main() {
+	//print("Hello world" ) 
+
+	println("Welcome to our comference")
+
+	var conferenceName = "Go conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	println("welcome to", conferenceName, "booking application")
+	print("We have total of tickets and  are still available.\n", conferenceTickets, remainingTickets)
+}
